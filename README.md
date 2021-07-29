9bcc5a70c827912d66dc6c115a85fad36ac0bcc8
xuecheng-1 <3045725155@qq.com>
2021-07-29 14:40:22 +0800

    blog

--- README.md
+++
@@ -1,1 +0,0 @@
-# xuecheng-1
--- 第三次作业/xuecheng/jquery/gotop.js
+++
@@ -1,5 +0,0 @@
- $("#gotop").click(function () {
-        var speed=200;//滑动的速度
-        $('body,html').animate({ scrollTop: 0 }, speed);
-        return false;
-    });
